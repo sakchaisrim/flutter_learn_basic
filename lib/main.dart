6384255756999a23489f6676b1edeee60b4ff9a1
sakchaisrim <sakchaisrim@gmail.com>
2023-11-23 11:40:44 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,67 @@ class MyApp extends StatelessWidget {
   String s = "LPRU";
   bool b = true;
   double d = 3.14;
+  int age = 20;
+
+  void geeting(){
+    print("Hello Funtion/Method");
+  }
+  // method with parameter
+  void person(String name){
+    print("Hello " + name);
+  }
+
+  // method with return
+  int add(int a, int b){
+    int sum;
+    sum = a + b;
+    return sum;
+  }
+
+  // Data Structure
+
+  // order list
+  List<String> colors = ["red", "blue", "yellow"];
+  
+  // unige element
+  Set<String> uniqeNames = {"dan", "bo", "vit"};
+
+  // map store key-value pairs
+  Map user = {
+    'name' : "Samit",
+    'age' : 25,
+    'height' : 160,
+    'weigh' : 55,
+  };
 
   @override
   Widget build(BuildContext context) {
+    // map
+    print(user);
+    print(user['age']);
+    
+    // set
+    print(uniqeNames);
+
+    // list
+    print(colors);
+    print(colors[0]);
+    // call method with return
+    print(add(5, 10));
+    // call method with parameter
+    person("sakchai");
+    // call method
+    geeting();
+    // confition
+    if(age >=20){
+      print("You are adult");
+    } else{
+      print("You are not adult");
+    }
+    // control 
+    for (var i = 0; i < 5; i++) {
+      print(i);
+    }
     print(a);
     print(s);
     print(b);
